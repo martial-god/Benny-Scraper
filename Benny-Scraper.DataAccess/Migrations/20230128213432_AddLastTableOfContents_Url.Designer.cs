354b451be3635a50128a4d2d398c50303257f0a7
Henry F <mnmathgeek1@gmail.com>
2023-01-28 15:35:33 -0600

    Dev: Add LastTableOfContentsUrl to Novel in order to have place to start searching for latestest chapters.

--- Benny-Scraper.DataAccess/Migrations/20230128213432_AddLastTableOfContents_Url.Designer.cs
+++ Benny-Scraper.DataAccess/Migrations/20230128213432_AddLastTableOfContents_Url.Designer.cs
@@ -4,6 +4,7 @@ using Benny_Scraper.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BennyScraper.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230128213432_AddLastTableOfContents_Url")]
+    partial class AddLastTableOfContentsUrl
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
