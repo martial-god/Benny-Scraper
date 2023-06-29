d2dfa924b0b618e7edbfb0540fc848d10a1b6755
Henry F <mnmathgeek1@gmail.com>
2023-06-29 07:25:37 -0500

    adding mangaKatana.com

---
+++ Benny-Scraper.BusinessLogic/Scrapers/Strategy/MangaKatanaStrategy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benny_Scraper.BusinessLogic.Scrapers.Strategy
+{
+    internal class MangaKatanaStrategy
+    {
+    }
+}
