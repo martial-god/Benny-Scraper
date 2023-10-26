ee50b759c71b209715b20e26c8df1baf0e0de515
Henry Feahn <hfeahn@poscorp.com>
2023-10-26 11:27:15 -0500

    Added migrations needed to avoid errors for renaming columns.

---
+++ Benny-Scraper.DataAccess/Migrations/20231026162640_RenameNovelColums.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BennyScraper.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameNovelColums : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SavedFileIsSplit",
+                table: "novel",
+                newName: "saved_file_is_split");
+
+            migrationBuilder.RenameColumn(
+                name: "FileType",
+                table: "novel",
+                newName: "file_type");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "saved_file_is_split",
+                table: "novel",
+                newName: "SavedFileIsSplit");
+
+            migrationBuilder.RenameColumn(
+                name: "file_type",
+                table: "novel",
+                newName: "FileType");
+        }
+    }
+}
